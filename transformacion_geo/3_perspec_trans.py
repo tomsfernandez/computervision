1cefe4c79a0b9502aa62aff9f782f79145870cf6
Tomás Fernández <tomas.fernandez@greencodesoftware.com>
2019-04-06 19:26:40 -0300

    import fix in geo trans 2 and uploaded 3_perspec_transform, needs checking

--- transformacion_geo/3_perspec_trans.py
+++ transformacion_geo/3_perspec_trans.py
@@ -8,7 +8,7 @@ image = cv2.imread('../resources/door.jpg')
 image = cv2.resize(image, (500, 700))
 target_image = cv2.imread('../resources/slant_door.jpg')
 target_image = cv2.resize(target_image, (500, 700))
-GREEN_SCALAR = (0, 0, 255)
+RED_SCALAR = (0, 0, 255)
 rows,cols,channels = image.shape
 base_param = (base_coor_queue, image, lambda: cv2.imshow("Base Image", image))
 target_param = (target_coor_queue, target_image, lambda: cv2.imshow("Target Image", target_image))
@@ -17,13 +17,13 @@ target_param = (target_coor_queue, target_image, lambda: cv2.imshow("Target Imag
 def on_mouse(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         set_point(x, y, param[0])
-        cv2.circle(param[1], (x, y), 3, GREEN_SCALAR, -1, 8, 0)
+        cv2.circle(param[1], (x, y), 3, RED_SCALAR, -1, 8, 0)
         print('x = %d, y = %d' % (x, y))
         param[2]()
 
 
 def set_point(x, y, queue):
-    if len(queue) == 3:
+    if len(queue) == 4:
         queue.popleft()
     queue.append((x, y))
 
@@ -31,7 +31,8 @@ def get_affine_array(array):
     first = [array[0][0], array[0][1]]
     second = [array[1][0], array[1][1]]
     third = [array[2][0], array[2][1]]
-    return np.float32([first,second,third])
+    fourth = [array[3][0], array[3][1]]
+    return np.float32([first,second,third, fourth])
 
 cv2.imshow("Base Image", image)
 cv2.imshow("Target Image", target_image)
@@ -40,14 +41,14 @@ cv2.setMouseCallback('Target Image', on_mouse, target_param)
 while True:
 
     if cv2.waitKey(1) & 0xFF == ord('c'):
-        if len(base_coor_queue) < 3 or len(target_coor_queue) < 3:
-            print("Eliga 3 coordenadas en cada imagen")
+        if len(base_coor_queue) < 4 or len(target_coor_queue) < 4:
+            print("Eliga 4 coordenadas en cada imagen")
         else: 
             pts1 = get_affine_array(base_coor_queue)
             pts2 = get_affine_array(target_coor_queue)
-            transform = cv2.getAffineTransform(pts1,pts2)
+            transform = cv2.getPerspectiveTransform(pts1,pts2)
             print(transform)
-            result = cv2.warpAffine(image, transform,(cols, rows))
+            result = cv2.warpPerspective(image, transform,(cols, rows))
             cv2.imshow("Result Image", result)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):

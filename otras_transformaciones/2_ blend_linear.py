14b41789e68527fe118d6ede7644cdd7dae49c01
Nicolas Velazquez <nicolas.velazquez@ing.austral.edu.ar>
2019-04-07 20:12:43 -0300

    Distance Transform

--- otras_transformaciones/2_ blend_linear.py
+++ otras_transformaciones/2_ blend_linear.py
@@ -1,13 +1,13 @@
-import cv2 as cv
+import cv2
 
-src1 = cv.imread(cv.samples.findFile('../resources/beach.jpg'))
-src2 = cv.imread(cv.samples.findFile('../resources/family.jpg'))
+src1 = cv2.imread(cv2.samples.findFile('../resources/beach.jpg'))
+src2 = cv2.imread(cv2.samples.findFile('../resources/family.jpg'))
 
 alpha = 0.5
 beta = (1.0 - alpha)
-dst = cv.addWeighted(src1, alpha, src2, beta, 0.0)
+dst = cv2.addWeighted(src1, alpha, src2, beta, 0.0)
 
-cv.imshow('Blend Linear', dst)
-cv.waitKey(0)
+cv2.imshow('Blend Linear', dst)
+cv2.waitKey(0)
 
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
---
+++ otras_transformaciones/3_distance_transform.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+video_capture = cv2.VideoCapture(0)
+
+
+while True:
+    ret, frame = video_capture.read()
+    grey_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    dist_img = cv2.distanceTransform(grey_image, cv2.DIST_L2, 5).astype(np.uint8)
+
+    cv2.imshow('Distance Transform', dist_img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break

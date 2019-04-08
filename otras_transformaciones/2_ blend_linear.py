38190c50ae0021567e806489c5a4a56ca601d7f4
Nicolas Velazquez <nicolas.velazquez@ing.austral.edu.ar>
2019-04-08 18:54:05 -0300

    Minor changes

--- otras_transformaciones/2_ blend_linear.py
+++ otras_transformaciones/2_ blend_linear.py
@@ -3,7 +3,7 @@ import cv2
 src1 = cv2.imread(cv2.samples.findFile('../resources/beach.jpg'))
 src2 = cv2.imread(cv2.samples.findFile('../resources/family.jpg'))
 
-alpha = 0.5
+alpha = 0.8
 beta = (1.0 - alpha)
 dst = cv2.addWeighted(src1, alpha, src2, beta, 0.0)
 
--- otras_transformaciones/3_distance_transform.py
+++ otras_transformaciones/3_distance_transform.py
@@ -6,6 +6,7 @@ video_capture = cv2.VideoCapture(0)
 
 while True:
     ret, frame = video_capture.read()
+    # Esto deberia ser con el threshold
     grey_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     dist_img = cv2.distanceTransform(grey_image, cv2.DIST_L2, 5).astype(np.uint8)
 

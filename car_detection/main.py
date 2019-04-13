2793d10f051467014c83ebf7705212dba33f4dbd
Tomás Fernández <tomas.fernandez@greencodesoftware.com>
2019-04-13 13:08:54 -0300

    unknown changes

--- car_detection/main.py
+++ car_detection/main.py
@@ -39,7 +39,7 @@ while video.isOpened():
     contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
-        draw_contour_center(contour, frame)
+#        draw_contour_center(contour, frame)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
     cv2.imshow("1. Grey", gray)
     cv2.imshow("2. Foreground", fgbg)
